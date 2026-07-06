288694e0e2671046598d1494a3b33380cc52e3bf
laurahott <laurahott@poli.ufrj.br>
2026-07-06 20:53:25 -0300

    Adiciona prog-22

---
+++ prog-22.py
@@ -0,0 +1,15 @@
+#ler 2 valores. usando if, verificar qual o maior. adicionar ele ao commit. dar push nele. verificar se push foi feito no git hub.
+#ler 2 valores. usando if, verificar qual o maior.
+#adicionar ele ao commit. dar push nele. verificar se push foi feito no github.
+
+n1 = int(input("fala o valor 1: "))
+n2 = int(input("fala o valor 2: "))
+
+if n1 > n2:
+    print(f"o número maior é {n1}")
+
+elif n2 > n1:
+    print(f"o número maior é {n2}")
+
+else:
+    print("os dois números são iguais")
